e9b3467bfc5803c662a823b5e527adee4441444f
mghalhku <ashashivu9@gmail.com>
2022-09-15 10:58:47 +0530

    3rdCommit

--- src/com/cg/demo/first_project.java
+++ src/com/cg/demo/first_project.java
@@ -5,5 +5,6 @@ public class first_project {
 	public static void main(String args[]) {
 		System.out.println("Back to java learnig");
 		System.out.println("all the best for learning");
+		System.out.println("Hello world welcome back");
 	}
 }
